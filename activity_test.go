d5ddd898e915777d3ba7594612b0fdd6274e04a2
Abram Van Der Geest <avanderg@tibco.com>
2019-03-14 13:09:48 -0400

    commenting out test

--- activity_test.go
+++ activity_test.go
@@ -1,13 +1,9 @@
 package resizeImage
 
 import (
-	"bufio"
-	"log"
-	"os"
 	"testing"
 
 	"github.com/project-flogo/core/activity"
-	"github.com/project-flogo/core/support/test"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -21,38 +17,38 @@ func TestRegister(t *testing.T) {
 
 func TestEval(t *testing.T) {
 
-	f, err := os.Open("/Users/avanderg@tibco.com/working/image_recog_hands_on/IMG_20190214_152236108.jpg")
-	if err != nil {
-		log.Fatal("trouble loading test file")
-	}
+	// f, err := os.Open("/Users/avanderg@tibco.com/working/image_recog_hands_on/IMG_20190214_152236108.jpg")
+	// if err != nil {
+	// 	log.Fatal("trouble loading test file")
+	// }
 
-	defer f.Close()
+	// defer f.Close()
 
-	fileInfo, _ := f.Stat()
-	var size int64 = fileInfo.Size()
-	bytes := make([]byte, size)
+	// fileInfo, _ := f.Stat()
+	// var size int64 = fileInfo.Size()
+	// bytes := make([]byte, size)
 
-	// read file into bytes
-	buffer := bufio.NewReader(f)
-	_, err = buffer.Read(bytes)
+	// // read file into bytes
+	// buffer := bufio.NewReader(f)
+	// _, err = buffer.Read(bytes)
 
-	//THIS TEST IS BROKEN CONVERT FILE TO []byte
+	// //THIS TEST IS BROKEN CONVERT FILE TO []byte
 
-	settings := &Settings{ResamplingFilter: "NearestNeighbor"}
+	// settings := &Settings{ResamplingFilter: "NearestNeighbor"}
 
-	iCtx := test.NewActivityInitContext(settings, nil)
-	act, err := New(iCtx)
-	assert.Nil(t, err)
-	input := &Input{MaxDimSize: 256, File: bytes}
-	tc := test.NewActivityContext(act.Metadata())
+	// iCtx := test.NewActivityInitContext(settings, nil)
+	// act, err := New(iCtx)
+	// assert.Nil(t, err)
+	// input := &Input{MaxDimSize: 256, File: bytes}
+	// tc := test.NewActivityContext(act.Metadata())
 
-	tc.SetInputObject(input)
+	// tc.SetInputObject(input)
 
-	done, err := act.Eval(tc)
-	assert.True(t, done)
-	assert.Nil(t, err)
+	// done, err := act.Eval(tc)
+	// assert.True(t, done)
+	// assert.Nil(t, err)
 
-	output := &Output{}
-	tc.GetOutputObject(output)
-	// fmt.Println(output.ResizedImage)
+	// output := &Output{}
+	// tc.GetOutputObject(output)
+	// // fmt.Println(output.ResizedImage)
 }
